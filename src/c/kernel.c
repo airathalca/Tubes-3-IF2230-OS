98534acc4c7562268e6e5379866cc927247615b7
airathalca <13520101@std.stei.itb.ac.id>
2022-02-23 21:14:54 +0700

    implemen readstring

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -5,3 +5,50 @@
 // TODO : Tambahkan implementasi kode C
 
 #include "header/kernel.h"
+
+int main() {
+    char buf[128];
+    clearScreen();
+    makeInterrupt21();
+    printString("Halo dunia!\r\n");
+    readString(buf);
+    printString(buf);
+
+    while (true);
+}
+
+void handleInterrupt21(int AX, int BX, int CX, int DX) {
+    switch (AX) {
+        case 0x0:
+            printString(BX);
+            break;
+        case 0x1:
+            readString(BX);
+            break;
+        default:
+            printString("Invalid Interrupt");
+    }
+}
+
+void printString(char *string) {
+    int i = 0;
+    char c = string[i];
+    while (c != '\0') {
+        interrupt(0x10, 0x0E00 + c,0,0,0);
+        i++;
+        c = string[i];
+    }
+}
+
+void readString(char *string) {
+    int i = 0;
+    char c = string[i];
+    while (c != '\0') {
+        interrupt(0x10, 0x0E00 + c,0,0,0);
+        i++;
+        c = string[i];
+    }
+}
+
+void clearScreen() {
+}
