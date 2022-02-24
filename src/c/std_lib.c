24cde310f2ea27184c364ec372f595761e8fbc6e
Aira Thalca <73151449+airathalca@users.noreply.github.com>
2022-02-24 21:07:50 +0700

    add div and mod function

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -1,3 +1,21 @@
 #include "header/std_lib.h"
 
 // Opsional : Tambahkan implementasi kode C
+int div (int a, int b) {
+  if (a == b) {
+    return 1;
+  }
+  int res = 0;
+  while (a >= b) {
+    a -= b;
+    res++;
+  }
+  return res;
+}
+
+int mod (int a, int n) {
+  while (a >= n) {
+    a -= n;
+  }
+  return a;
+}
