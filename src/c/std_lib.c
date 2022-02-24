ab7704822eaa4c8efc1ffc23ece085accf6749f7
airathalca <13520101@std.stei.itb.ac.id>
2022-02-24 21:30:17 +0700

    implemen strcmp and strlen

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -19,3 +19,24 @@ int mod (int a, int n) {
   }
   return a;
 }
+
+unsigned int strlen(char *string) {
+  int res = 0;
+  while (string[res] != 0x0) {
+    res++;
+  }
+  return res;
+}
+
+bool strcmp(char *s1, char *s2) {
+  if (strlen(s1) != strlen(s2)) {
+    return false;
+  }
+  int i;
+  for (i = 0;i < strlen(s1);i++) {
+    if (s1[i] != s2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
