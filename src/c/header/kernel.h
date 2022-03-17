1e6af4f8c62672d7101fbc62e812a965b1e057a8
IloveNooodles <mgarebaldhie80@gmail.com>
2022-03-18 01:46:50 +0700

    add template milestone 2

--- src/c/header/kernel.h
+++ src/c/header/kernel.h
@@ -4,13 +4,22 @@
 #include "std_lib.h"
 
 // Fungsi bawaan
-extern void putInMemory(int segment, int address, byte b);
+extern void putInMemory(int segment, int address, char character);
 extern int interrupt(int int_number, int AX, int BX, int CX, int DX);
-extern void makeInterrupt21();
-
+void makeInterrupt21();
 void handleInterrupt21(int AX, int BX, int CX, int DX);
+void fillKernelMap();
 
-// Implementasikan
+
+// TODO : Implementasikan
 void printString(char *string);
 void readString(char *string);
 void clearScreen();
+
+void writeSector(byte *buffer, int sector_number);
+void readSector(byte *buffer, int sector_number);
+
+void write(struct file_metadata *metadata, enum fs_retcode *return_code);
+void read(struct file_metadata *metadata, enum fs_retcode *return_code);
+
+void shell();
