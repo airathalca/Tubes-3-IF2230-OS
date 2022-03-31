78cbe10fb7e780f8d587e1ebe895c862b2fac12d
Aira Thalca <13520101@std.stei.itb.ac.id>
2022-03-31 21:17:03 +0700

    add header shell

---
+++ src/c/header/shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#define MAX_LEN 25
+
+void shell();
+
+void commandParser(char *command, char *argument);
+
+void cd(char *parentIndex, char *dir, char *newCWD);
+
+void ls(char parentIndex);
+
+void cat(char parentIndex, char *path);
+
+void cp(char parentIndex, char *resourcePath, char *destinationPath);
+
+#endif
