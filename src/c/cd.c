4307aab5a015ab363a52712ebee6d43c1b14050c
rayhankinan <rayhankinan@gmail.com>
2022-04-30 20:11:36 +0700

    Fix argument number

--- src/c/cd.c
+++ src/c/cd.c
@@ -15,23 +15,33 @@ int main() {
 
   getMessage(&m, getCurrentSegment());
 
-  if (m.arg2[0] == '\0') {
-    ret_code = FS_R_NODE_NOT_FOUND;
-    error_code(ret_code, m.arg1, m.arg2, m.arg3);
-
-  } else if (m.arg2[0] == '/') {
-    m.current_directory = read_relative_path(FS_NODE_P_IDX_ROOT, m.arg2 + 1, &ret_code);
-
-    if (ret_code != 0) {
+  if (m.valid) {
+    if(m.arg3[0] != '\0') {
+      error_code(9, m.arg1, m.arg2, m.arg3);
+      exit();
+    }
+    
+    if (m.arg2[0] == '\0') {
+      ret_code = FS_R_NODE_NOT_FOUND;
       error_code(ret_code, m.arg1, m.arg2, m.arg3);
+
+    } else if (m.arg2[0] == '/') {
+      m.current_directory = read_relative_path(FS_NODE_P_IDX_ROOT, m.arg2 + 1, &ret_code);
+
+      if (ret_code != 0) {
+        error_code(ret_code, m.arg1, m.arg2, m.arg3);
+      }
+
+    } else {
+      m.current_directory = read_relative_path(m.current_directory, m.arg2, &ret_code);
+
+      if (ret_code != 0) {
+        error_code(ret_code, m.arg1, m.arg2, m.arg3);
+      }
     }
 
   } else {
-    m.current_directory = read_relative_path(m.current_directory, m.arg2, &ret_code);
-
-    if (ret_code != 0) {
-      error_code(ret_code, m.arg1, m.arg2, m.arg3);
-    }
+    error_code(9, m.arg1, m.arg2, m.arg3);
   }
 
   sendMessage(&m, getCurrentSegment());
--- src/c/ls.c
+++ src/c/ls.c
@@ -16,46 +16,53 @@ int main(){
 
   readSector(&(node_fs_buffer.nodes[0]), FS_NODE_SECTOR_NUMBER);
   readSector(&(node_fs_buffer.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
   getMessage(&m, getCurrentSegment());
 
-  if(m.arg3[0] != '\0') {
-    error_code(9, m.arg1, m.arg2, m.arg3);
-    exit();
-  }
+  if (m.valid) {
+    if(m.arg3[0] != '\0') {
+      error_code(9, m.arg1, m.arg2, m.arg3);
+      exit();
+    }
 
-  if(m.arg2[0] == '\0') {
-    while (i < 64) {
-      if (strlen(node_fs_buffer.nodes[i].name) > 0 && node_fs_buffer.nodes[i].parent_node_index == m.current_directory) {
+    if(m.arg2[0] == '\0') {
+      while (i < 64) {
+        if (strlen(node_fs_buffer.nodes[i].name) > 0 && node_fs_buffer.nodes[i].parent_node_index == m.current_directory) {
+          puts(node_fs_buffer.nodes[i].name);
+          puts("\r\n");
+        }
+        i++;
+      }
+      ret_code = FS_SUCCESS;
+      exit();
+    }
+
+    //klo arg1 nya ga nol berarti nyari node yang namanya sama
+    for (i = 0; i < 64; i++) {
+      if (strlen(node_fs_buffer.nodes[i].name) > 0 && strcmp(node_fs_buffer.nodes[i].name, m.arg2) && node_fs_buffer.nodes[i].parent_node_index == m.current_directory){
+        parentFound = i;
+        break;
+      }
+    }
+
+    if (i == 64) {
+      ret_code = FS_W_INVALID_FOLDER;
+      error_code(ret_code, m.arg1, m.arg2, m.arg3);
+      exit();
+    }
+
+    //ls lagi sesuai folder yang baru
+    for (i = 0; i < 64; i++) {
+      if (strlen(node_fs_buffer.nodes[i].name) > 0 && node_fs_buffer.nodes[i].parent_node_index == parentFound) {
         puts(node_fs_buffer.nodes[i].name);
         puts("\r\n");
       }
-      i++;
     }
-    ret_code = FS_SUCCESS;
-    exit();
-  }
+    // ret_code = FS_SUCCESS;
 
-  //klo arg1 nya ga nol berarti nyari node yang namanya sama
-  for (i = 0; i < 64; i++) {
-    if (strlen(node_fs_buffer.nodes[i].name) > 0 && strcmp(node_fs_buffer.nodes[i].name, m.arg2) && node_fs_buffer.nodes[i].parent_node_index == m.current_directory){
-      parentFound = i;
-      break;
-    }
+  } else {
+    error_code(9, m.arg1, m.arg2, m.arg3);
   }
-
-  if (i == 64) {
-    ret_code = FS_W_INVALID_FOLDER;
-    error_code(ret_code, m.arg1, m.arg2, m.arg3);
-    exit();
-  }
-
-  //ls lagi sesuai folder yang baru
-  for (i = 0; i < 64; i++) {
-    if (strlen(node_fs_buffer.nodes[i].name) > 0 && node_fs_buffer.nodes[i].parent_node_index == parentFound) {
-      puts(node_fs_buffer.nodes[i].name);
-      puts("\r\n");
-    }
-  }
-  ret_code = FS_SUCCESS;
+  
   exit();
 }
